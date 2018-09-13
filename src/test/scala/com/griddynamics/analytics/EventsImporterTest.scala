1601d2081eb020121fbac4e7b57a47066ca87ee9
Filter94 <IamFilter94@gmail.com>
2018-09-13 11:49:29 +0300

    Fixed expected event in some trivial test case.

--- src/test/scala/com/griddynamics/analytics/EventsImporterTest.scala
+++ src/test/scala/com/griddynamics/analytics/EventsImporterTest.scala
@@ -11,7 +11,7 @@ class EventsImporterTest extends FunSuite with TrivialEventsImporter {
 
   test("Events imported correctly.") {
     val expectedEvent = Event("product name 1", 123.123f, Timestamp.valueOf("2018-09-11 12:00:00.123"),
-      "category 1", "127.0.0.1")
+      "category 1", "1.0.0.128")
     assert(eventsImporter.importData().collect()(0) == expectedEvent)
   }
 }
